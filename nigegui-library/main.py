72960ae1b85ebf7d74ce7d43ad2e39cea7e27e16
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2024-05-25 22:17:14 -0300

    :fire: Deleted old files

--- nigegui-library/main.py
+++
@@ -1,9 +0,0 @@
-from nicegui import ui
-
-with ui.card().tight():
-    ui.image('https://picsum.photos/id/684/640/360')
-
-    with ui.card_section():
-        ui.label('Lorem ipsum dolor sit amet, consectetur adipiscing elit, ...')
-
-ui.run()
